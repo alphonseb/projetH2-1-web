cc4b8579be1dea1b15e7881f91472040834d3f3d
alphonseb <alphonse.bouy@gmail.com>
2018-12-16 22:17:42 +0100

    :sparkles: started family member template for tree

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <Tree/>
     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tree from "./components/Tree.vue";
 
 export default {
-    name: 'app',
+    name: "app",
     components: {
-        HelloWorld
+        Tree
     }
-}
+};
 </script>
 
 <style lang="scss">
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+    font-family: "Avenir", Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
 </style>
