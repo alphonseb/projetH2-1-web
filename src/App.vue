2ec3c0bfea964f1ce4a52e1a99c1f3eb7a331945
alphonseb <alphonse.bouy@gmail.com>
2018-12-17 22:30:35 +0100

    :lipstick: formatting

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <!-- La navigation se faire avec des router-link, c'est comme des a, mais pour vue -->
-      <!-- on met dans le to le path renseigner dans le fichier router.js -->
-      <!-- les composants changeront dans le router view, donc on met le footer et nav autour  -->
-      <!-- comme c'est le cas ici si ce n'est qu'il faut adapter -->
-      <!-- quand le nav/footer devra ne pas appraitre, on mettra des v-if en fonction de la route -->
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <div id="nav">
+            <!-- La navigation se faire avec des router-link, c'est comme des a, mais pour vue -->
+            <!-- on met dans le to le path renseigner dans le fichier router.js -->
+            <!-- les composants changeront dans le router view, donc on met le footer et nav autour  -->
+            <!-- comme c'est le cas ici si ce n'est qu'il faut adapter -->
+            <!-- quand le nav/footer devra ne pas appraitre, on mettra des v-if en fonction de la route -->
+            <router-link to="/">Home</router-link>|
+            <router-link to="/about">About</router-link>
+        </div>
+        <router-view/>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
--- src/router.js
+++ src/router.js
@@ -6,7 +6,7 @@ Vue.use(Router)
 
 /*
 Ici on ajoute nos route, c'est la que la navigation ce se fait en fonction du composant
-pour les routes: path: 
+pour les routes: path:
   ce qu'il y aura dans l'url
   name: nom de la route (= non du composant en gros)
   component: vous importez votr component,
@@ -16,14 +16,14 @@ le dossier component servira pour les composants utiliser dans les views
 Suite sur App.vue
 */
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      // Pour dev mettez votre composant dans le path / (la racine du site)
-      path: '/',
-      name: 'landing',
-      component: Landing
-    }
-  ]
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            // Pour dev mettez votre composant dans le path / (la racine du site)
+            path: '/',
+            name: 'landing',
+            component: Landing
+        }
+    ]
 })
