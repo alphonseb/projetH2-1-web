c73a9b6156252db0eca574afd4323673c32c18e6
TheoFenique <theo.fenique@gmail.com>
2018-12-17 14:09:33 +0100

    :sparkles: Add profil edit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
-    <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png">
-        <HelloWorld msg="Welcome to Your Vue.js App" />
-    </div>
+    <profile></profile>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import profile from './components/Profile.vue'
 
 export default {
     name: 'app',
     components: {
-        HelloWorld
+        profile
     }
 }
 </script>
 
 <style lang="scss">
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
+    body{
+        margin: 0
     }
 </style>
