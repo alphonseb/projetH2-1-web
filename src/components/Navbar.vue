679c3ea30f16c2bd103210bb2d21f9cb71c615d4
alphonseb <alphonse.bouy@gmail.com>
2018-12-19 01:40:04 +0100

    :sparkles: templating for nav component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="navbar">
+        <nav></nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
+
+
