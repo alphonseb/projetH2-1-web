166f69aad00b0517abb15c0b2956a1b7c003e8c4
Jules Guesnon <jules.guesnon@hetic.net>
2018-12-19 02:43:34 +0100

    :sparkles: Created tag input

--- src/components/TagInput.vue
+++ src/components/TagInput.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="tagInput">
         <div class="tags">
-            <div class="tag"></div>
+            <div class="tag" v-for="(tag, i) in tags" :key="i" @click="deleteTag(tag)"> <span>{{ tag }}</span> <span class="delete">X</span></div>
         </div>
-        <input type="text" v-model="tag" :placeholder="placeholder" @keypress.enter="addTag">
+        <input type="text" v-model="tag" :placeholder="getPlaceholder" @keypress.enter="addTag" @keydown.delete="deleteLastTag">
     </div>
 </template>
 
@@ -20,8 +20,25 @@ export default {
     },
     methods: {
         addTag () {
+            if (this.tag === '')
+                return
+
             this.tags.push(this.tag)
             this.tag = ''
+        },
+        deleteTag (_tag) {
+            this.tags = this.tags.filter(t => t !== _tag)
+        },
+        deleteLastTag () {
+            if (this.tag !== '' || this.tags.length === 0)
+                return
+
+            this.tags.splice(this.tags.length - 1, 1)
+        }
+    },
+    computed: {
+        getPlaceholder () {
+            return this.tags.length > 0 ? '': this.placeholder
         }
     }
 }
@@ -34,14 +51,44 @@ export default {
     height: 25px;
     border-bottom: 1px solid white;
     margin-bottom: 20px;
+    display: flex;
 
+    .tags {
+        width: auto;
+        height: 100%;
+        display: flex;
+
+        .tag {
+            position: relative;
+            background: white;
+            color: black;
+            border-radius: 100px;
+            padding: 5px 15px 5px 15px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin-left: 5px;
+
+            .delete {
+                position: absolute;
+                right: 3px;
+                top: 50%;
+                transform: translateY(-50%);
+                font-size: 13px;
+            }
+        }
+    }
     input {
-        width: calc(100% - 6px);
         height: calc(100% - 6px);
         padding: 3px;
         border: none;
         background: none;
         font-size: 1.2em;
+        color: white;
+
+        &:focus {
+            outline-width: 0;
+        }
     }
 }
 </style>
